3ea944fbbd9cfd6379b6f8b8851afe0067ef7e13
dinno <tahadlrb7@gmail.com>
2025-11-28 21:00:27 +0330

    fixup! feat(auth): add jwt configs

--- src/modules/auth/application/auth.module.ts
+++ src/modules/auth/application/auth.module.ts
@@ -3,11 +3,15 @@ import { HashingModule } from 'src/shared';
 import { AuthController } from '../presenters/http/auth.controller';
 import { SignupUC } from './usecases/signup.uc';
 import { AuthService } from './auth.service';
+import { UsersInfrastructureModule } from 'src/modules/users/infrastructure/users-infrastructure.module';
 import { UsersModule } from 'src/modules/users/application/users.module';
 import { SigninUC } from './usecases/signin.uc';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from './configs';
 
 @Module({
   imports: [
+    JwtModule.registerAsync(jwtConfig.asProvider()),
     UsersModule,
     HashingModule,
   ],
