bf48e984ab88e28a8a05dfd3c18e7eb43b2d2294
dinno <tahadlrb7@gmail.com>
2025-11-28 21:23:37 +0330

    feat(signin): generate tokens

--- src/modules/auth/application/usecases/signin.uc.ts
+++ src/modules/auth/application/usecases/signin.uc.ts
@@ -2,15 +2,18 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { HashingService } from 'src/shared';
 import { UsersService } from 'src/modules/users/application/users.service';
 import { SignInDto } from '../../dtos/sing-in.dto';
+import { AuthTokenService } from '../auth-token.service';
+import { TokensGenerated } from '../types';
 
 @Injectable()
 export class SigninUC {
   constructor(
     private readonly userService: UsersService,
     private readonly hashingService: HashingService,
+    private readonly authTokenService: AuthTokenService,
   ) {}
 
-  async execute({ email, password }: SignInDto): Promise<void> {
+  async execute({ email, password }: SignInDto): Promise<TokensGenerated> {
     const user = await this.userService.getByEmail(email);
     const dummyPassword = this.hashingService.genDummyPassword();
 
@@ -24,6 +27,7 @@ export class SigninUC {
         'Combination of email & password is not correct!',
       );
     }
-    // ...
+    const tokens = await this.authTokenService.generateJWTTokens(user.id);
+    return tokens;
   }
 }
