04e9f139cca8aee1d740c36f05891c776bffa7ee
dinno <tahadlrb7@gmail.com>
2025-11-28 20:41:26 +0330

    feat(auth): add signin/signup dto

---
+++ src/modules/auth/dtos/sing-up.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
