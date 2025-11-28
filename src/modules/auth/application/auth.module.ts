63e5265d0b3449c1e31d49abfc3adf175aae784e
dinno <tahadlrb7@gmail.com>
2025-11-28 21:24:26 +0330

    chore(auth): update module

--- src/modules/auth/application/auth.module.ts
+++ src/modules/auth/application/auth.module.ts
@@ -3,19 +3,21 @@ import { HashingModule } from 'src/shared';
 import { AuthController } from '../presenters/http/auth.controller';
 import { SignupUC } from './usecases/signup.uc';
 import { AuthService } from './auth.service';
-import { UsersInfrastructureModule } from 'src/modules/users/infrastructure/users-infrastructure.module';
 import { UsersModule } from 'src/modules/users/application/users.module';
 import { SigninUC } from './usecases/signin.uc';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from './configs';
+import { AuthTokenService } from './auth-token.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forFeature(jwtConfig),
     JwtModule.registerAsync(jwtConfig.asProvider()),
     UsersModule,
     HashingModule,
   ],
-  providers: [AuthService, SignupUC, SigninUC],
+  providers: [AuthService, AuthTokenService, SignupUC, SigninUC],
   controllers: [AuthController],
 })
 export class AuthModule {}
