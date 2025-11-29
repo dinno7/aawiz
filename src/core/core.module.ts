791dc0c8d4a8d7ec73f0511bcf68a9abf5dc10ad
dinno <tahadlrb7@gmail.com>
2025-11-29 23:19:41 +0330

    feat(mikro orm): define & import configuration

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,16 +1,22 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import mikroOrmConfig from '../mikro-orm.config';
-import { envsValidator } from '../envs';
+import getMikroOrmConfig from '../mikro-orm.config';
+import { envsValidator, NodeEnv } from 'src/envs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === NodeEnv.prod ? '.env.prod' : '.env.dev',
       validate: envsValidator,
     }),
-    MikroOrmModule.forRoot(mikroOrmConfig),
+    MikroOrmModule.forRoot(
+      getMikroOrmConfig('app', {
+        POSTGRES_URI: process.env.POSTGRES_URI,
+      }),
+    ),
   ],
 })
 export class CoreModule {}
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,24 +1,50 @@
+import { defineConfig } from '@mikro-orm/core';
 import { Migrator } from '@mikro-orm/migrations';
 import { Options, PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { Envs, NodeEnv } from './envs';
 
-const config: Options = {
-  driver: PostgreSqlDriver,
-  dbName: 'aawiz',
-  user: 'dinno',
-  extensions: [Migrator],
-  password: '1234',
-  entities: ['dist/**/*.entity.js'],
-  entitiesTs: ['src/**/*.entity.ts'],
-  debug: true,
-  migrations: {
-    path: 'dist/migrations',
-    pathTs: 'src/migrations',
-    fileName(timestamp: string, name?: string): string {
-      if (!name || name?.length === 0) {
-        throw new Error('Please provide --name');
-      }
-      return `${timestamp}_${name?.replace(/\s/g, '_')}`;
+function getMikroOrmConfig(contextName: undefined): Options;
+function getMikroOrmConfig(contextName: 'cli'): Options;
+function getMikroOrmConfig(
+  contextName: 'app',
+  envs: Pick<Envs, 'POSTGRES_URI'>,
+): Options;
+function getMikroOrmConfig(
+  contextName?: 'cli' | 'app',
+  envs?: Pick<Envs, 'POSTGRES_URI'>,
+) {
+  const config = defineConfig({
+    driver: PostgreSqlDriver,
+    extensions: [Migrator],
+    clientUrl: process.env.POSTGRES_URI,
+    entities: ['dist/**/*.entity.js'],
+    entitiesTs: ['src/**/*.entity.ts'],
+    debug: process.env.NODE_ENV !== NodeEnv.prod, // Only debug in non-production environments
+    migrations: {
+      path: 'dist/migrations',
+      pathTs: 'src/migrations',
+      fileName(timestamp: string, name?: string): string {
+        if (!name || name?.length === 0) {
+          throw new Error('Please provide --name');
+        }
+        return `${timestamp}_${name?.replace(/\s/g, '_')}`;
+      },
     },
-  },
-};
-export default config;
+  });
+
+  if (contextName === 'app') {
+    if (!envs) {
+      throw new Error("envs is required when contextName is 'app'");
+    }
+    config.clientUrl = envs.POSTGRES_URI;
+    return config;
+  } else {
+    // Load .envs for cli
+    require?.('dotenv')?.config?.();
+    config.clientUrl = process.env.POSTGRES_URI;
+  }
+
+  return config;
+}
+
+export default getMikroOrmConfig;
