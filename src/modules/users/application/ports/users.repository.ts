a73ff7f36ce7a46103cfdfb4def41139b2e69880
dinno <tahadlrb7@gmail.com>
2025-11-30 14:06:22 +0330

    feat(users): add & use UserPublic type

--- src/modules/users/application/ports/users.repository.ts
+++ src/modules/users/application/ports/users.repository.ts
@@ -1,8 +1,9 @@
 import { UUID } from 'crypto';
-import { User, UserForCreate } from '../../domain';
+import { User, UserForCreate, UserPublic } from '../../domain';
 
 export abstract class UserRepository {
-  abstract create(user: UserForCreate): Promise<User>;
-  abstract getById(id: UUID): Promise<User | null>;
-  abstract getByEmail(email: string): Promise<User | null>;
+  abstract create(user: UserForCreate): Promise<UserPublic>;
+  abstract getById(id: UUID): Promise<UserPublic | null>;
+  abstract getByEmail(email: string): Promise<UserPublic | null>;
+  abstract getByEmailWithPassword(email: string): Promise<User | null>;
 }
