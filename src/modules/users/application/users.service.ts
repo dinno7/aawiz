68a1639f94d0e8c3b9512b5b8398e6e1128b36f1
dinno <tahadlrb7@gmail.com>
2025-11-30 14:06:22 +0330

    feat(users): add & use UserPublic type

--- src/modules/users/application/users.service.ts
+++ src/modules/users/application/users.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './ports';
-import { User, UserForCreate } from '../domain';
+import { User, UserForCreate, UserPublic } from '../domain';
 import { UUID } from 'crypto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async createUser(user: UserForCreate): Promise<User> {
+  async createUser(user: UserForCreate): Promise<UserPublic> {
     return this.userRepository.create(user);
   }
 
-  async getById(id: UUID): Promise<User | null> {
+  async getById(id: UUID): Promise<UserPublic | null> {
     return this.userRepository.getById(id);
   }
 
-  async getByEmail(email: string): Promise<User | null> {
+  async getByEmail(email: string): Promise<UserPublic | null> {
     return this.userRepository.getByEmail(email);
+  }
+
+  async getByEmailForAuth(email: string): Promise<User | null> {
+    return this.userRepository.getByEmailWithPassword(email);
   }
 
   async isUserEmailExists(email: string): Promise<boolean> {
--- src/modules/users/domain/types.ts
+++ src/modules/users/domain/types.ts
@@ -1,3 +1,5 @@
 import { User } from './users';
 
 export type UserForCreate = Pick<User, 'name' | 'email' | 'password'>;
+
+export type UserPublic = Omit<User, 'password'>;
