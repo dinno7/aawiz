38fdf75c4d98920c6b64f67231ca51a2c47f7288
dinno <tahadlrb7@gmail.com>
2025-11-30 14:14:48 +0330

    chore: update Express types & add user to request

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,13 @@
 import { Envs } from './envs';
+import { UserPublic } from './modules/users/domain';
 
 declare global {
   declare namespace NodeJS {
     interface ProcessEnv extends Envs {}
+  }
+  declare namespace Express {
+    interface Request {
+      user?: UserPublic;
+    }
   }
 }
