d27150eba06f75af1bceaf3913d5e87101079e33
dinno <tahadlrb7@gmail.com>
2025-11-30 14:10:26 +0330

    feat(auth): use commands & implement refresh token flow stuff

--- src/modules/auth/application/auth.service.ts
+++ src/modules/auth/application/auth.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { SignupUC } from './usecases/signup.uc';
-import { SignUpDto } from '../dtos/sing-up.dto';
-import { User } from 'src/modules/users/domain';
-import { SignInDto } from '../dtos/sing-in.dto';
+import { UserPublic } from 'src/modules/users/domain';
 import { SigninUC } from './usecases/signin.uc';
-import { TokensGenerated } from './types';
+import { SigninCommand } from './commands/signin.command';
+import { SignupCommand } from './commands/signup.command';
+import { SigninResult } from './types';
+import { RefreshTokensUC } from './usecases/refresh-tokens.uc';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly signupUC: SignupUC,
     private readonly signinUC: SigninUC,
+    private readonly refreshTokenUC: RefreshTokensUC,
   ) {}
 
-  async signup(user: SignUpDto): Promise<User> {
+  async signup(user: SignupCommand): Promise<UserPublic> {
     return this.signupUC.execute(user);
   }
 
-  async signin(user: SignInDto): Promise<TokensGenerated> {
+  async signin(user: SigninCommand): Promise<SigninResult> {
     return this.signinUC.execute(user);
+  }
+
+  async refreshTokens(refreshToken: string): Promise<SigninResult> {
+    return this.refreshTokenUC.execute(refreshToken);
   }
 }
