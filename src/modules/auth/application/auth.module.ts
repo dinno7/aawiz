0af320e94e2050a4f014b18602ca0b1000c70c27
dinno <tahadlrb7@gmail.com>
2025-11-30 14:11:36 +0330

    refactor(auth): update module

--- src/modules/auth/application/auth.module.ts
+++ src/modules/auth/application/auth.module.ts
@@ -9,6 +9,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from './configs';
 import { AuthTokenService } from './auth-token.service';
 import { ConfigModule } from '@nestjs/config';
+import { MemoryStorageModule } from 'src/shared/memory-storage';
+import { BearerGuard } from '../presenters/http/guards/bearer.guard';
+import { AuthenticationGuard } from '../presenters/http/guards/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { RefreshTokensUC } from './usecases/refresh-tokens.uc';
 
 @Module({
   imports: [
@@ -16,8 +21,20 @@ import { ConfigModule } from '@nestjs/config';
     JwtModule.registerAsync(jwtConfig.asProvider()),
     UsersModule,
     HashingModule,
+    MemoryStorageModule,
   ],
-  providers: [AuthService, AuthTokenService, SignupUC, SigninUC],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    AuthService,
+    AuthTokenService,
+    BearerGuard,
+    SignupUC,
+    SigninUC,
+    RefreshTokensUC,
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
