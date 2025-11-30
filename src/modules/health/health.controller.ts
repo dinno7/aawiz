f194850f0948c1644e3410593c8e28b4e5526ef1
dinno <tahadlrb7@gmail.com>
2025-11-30 14:14:30 +0330

    feat(health): make public

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { CheckHealthResDto } from './dtos/check-health.dto';
 import { HealthCheckService } from './health.service';
+import { Auth } from '../auth/presenters/http/decorators/auth.decorator';
+import { AuthTypes } from '../auth/presenters/http/types';
 
 @Controller('health')
 export class HealthCheckController {
   constructor(private readonly healthCheckService: HealthCheckService) {}
 
   @Get()
+  @Auth(AuthTypes.None)
   @ApiOkResponse({
     type: CheckHealthResDto,
     description: 'Checking application health',
