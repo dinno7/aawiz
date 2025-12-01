44aabd51533d5e587a6bf665f46675bb100f807d
dinno <tahadlrb7@gmail.com>
2025-12-01 16:38:34 +0330

    feat(app error): set default message

--- src/shared/errors/app.error.ts
+++ src/shared/errors/app.error.ts
@@ -2,4 +2,7 @@ import { ErrorTypes } from '../types';
 
 export class AppError extends Error {
   type: ErrorTypes = ErrorTypes.APP;
+  get message(): string {
+    return 'Something went wrong in application, please try again';
+  }
 }
