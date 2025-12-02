8fa63527c638e03b304b55ed6b4bba19db417dbe
dinno <tahadlrb7@gmail.com>
2025-12-02 16:36:02 +0330

    feat(evaluations): implement repository with mikro orm

--- src/modules/evaluations/infrastructure/persistence/mikroorm/evaluations-mikroorm.repository.ts
+++ src/modules/evaluations/infrastructure/persistence/mikroorm/evaluations-mikroorm.repository.ts
@@ -1,12 +1,18 @@
 import { EntityManager } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
+
 import { EvaluationRepository } from 'src/modules/evaluations/application/ports/evaluation.repository';
-import { CreateEvaluationInput } from 'src/modules/evaluations/application/types/evaluation';
+import {
+  CreateEvaluationInput,
+  UpdateEvaluationInput,
+} from 'src/modules/evaluations/domain/types';
 import { Evaluation } from 'src/modules/evaluations/domain/eveluation';
 import { MikroOrmEvaluationMapper } from './evaluations-mikroorm.mapper';
 import { MikroORMEvaluation } from './evaluations-mikroorm.entity';
 import { UUID } from 'crypto';
 import { EvaluationStatus } from 'src/modules/evaluations/domain/enums';
+import { EvaluationNotFoundError } from 'src/modules/evaluations/domain';
+import { pickDefined } from 'src/shared';
 
 @Injectable()
 export class MikroORMEvaluationRepository implements EvaluationRepository {
@@ -25,5 +31,36 @@ export class MikroORMEvaluationRepository implements EvaluationRepository {
     });
     await this.em.flush();
     return MikroOrmEvaluationMapper.toDomain(newEvaluate);
+  }
+
+  async getById(id: UUID): Promise<Evaluation> {
+    const fetchedEval = await this.em.findOne(MikroORMEvaluation, id);
+    if (!fetchedEval) {
+      throw new EvaluationNotFoundError();
+    }
+    return MikroOrmEvaluationMapper.toDomain(fetchedEval);
+  }
+
+  async updateById(
+    id: UUID,
+    input: Partial<UpdateEvaluationInput>,
+  ): Promise<Evaluation> {
+    const fetchedEval = await this.em.findOne(MikroORMEvaluation, id);
+    if (!fetchedEval) {
+      throw new EvaluationNotFoundError();
+    }
+    Object.assign(fetchedEval, pickDefined(input));
+    await this.em.flush();
+
+    return MikroOrmEvaluationMapper.toDomain(fetchedEval);
+  }
+
+  async deleteById(id: UUID): Promise<boolean> {
+    const isExists = await this.em.findOne(MikroORMEvaluation, id);
+    if (!isExists) {
+      throw new EvaluationNotFoundError();
+    }
+    await this.em.removeAndFlush(isExists);
+    return true;
   }
 }
