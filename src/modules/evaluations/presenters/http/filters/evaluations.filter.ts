161922634294e5207f826a39ccefb4feb8fdf46c
dinno <tahadlrb7@gmail.com>
2025-12-02 16:37:21 +0330

    refactor(evaluations): remove filter

--- src/modules/evaluations/presenters/http/filters/evaluations.filter.ts
+++
@@ -1,9 +0,0 @@
-import { BadRequestException, Catch, ExceptionFilter } from '@nestjs/common';
-import { AppError } from 'src/shared';
-
-@Catch(AppError)
-export class EvaluationsExceptionFilter implements ExceptionFilter {
-  catch(exception: AppError) {
-    throw new BadRequestException(exception.message);
-  }
-}
