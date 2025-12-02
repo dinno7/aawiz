fcca0998e7a1c0454c25271c5b107eb75d936401
dinno <tahadlrb7@gmail.com>
2025-12-02 14:37:50 +0330

    feat(evaluations,application): add delete,update,read one repository methods

--- src/modules/evaluations/application/ports/evaluation.repository.ts
+++ src/modules/evaluations/application/ports/evaluation.repository.ts
@@ -1,5 +1,14 @@
+import { UUID } from 'crypto';
 import { Evaluation } from '../../domain/eveluation';
-import { CreateEvaluationInput } from '../types/evaluation';
+import { CreateEvaluationInput } from '../types';
+import { UpdateEvaluationInput } from '../../domain/types';
+
 export abstract class EvaluationRepository {
   abstract create(inpEval: CreateEvaluationInput): Promise<Evaluation>;
+  abstract getById(id: UUID): Promise<Evaluation>;
+  abstract updateById(
+    id: UUID,
+    input: Partial<UpdateEvaluationInput>,
+  ): Promise<Evaluation>;
+  abstract deleteById(id: UUID): Promise<boolean>;
 }
