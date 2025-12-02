717822e21192721111ee615afaaf5460b0c1794f
dinno <tahadlrb7@gmail.com>
2025-12-02 16:37:31 +0330

    feat(evaluations): sync module

--- src/modules/evaluations/application/evaluations.module.ts
+++ src/modules/evaluations/application/evaluations.module.ts
@@ -3,10 +3,22 @@ import { EvaluationsController } from '../presenters/http/evaluation.controller'
 import { EvaluationsInfrastructureModule } from '../infrastructure/persistence/evaluations-infrastructure.module';
 import { CreateEvaluationUC } from './usecases/create-evaluation.uc';
 import { EvaluationsService } from './evaluations.service';
+import {
+  DeleteEvaluationUC,
+  ReadOneEvaluationUC,
+  UpdateEvaluationUC,
+} from './usecases';
+import { UsersModule } from 'src/modules/users/application/users.module';
 
 @Module({
-  imports: [EvaluationsInfrastructureModule],
+  imports: [UsersModule, EvaluationsInfrastructureModule],
   controllers: [EvaluationsController],
-  providers: [EvaluationsService, CreateEvaluationUC],
+  providers: [
+    EvaluationsService,
+    CreateEvaluationUC,
+    ReadOneEvaluationUC,
+    UpdateEvaluationUC,
+    DeleteEvaluationUC,
+  ],
 })
 export class EvaluationsModule {}
