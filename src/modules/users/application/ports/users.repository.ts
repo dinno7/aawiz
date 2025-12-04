ff5f750d6807b26119b6e70f4e70ffc534ca900b
dinno <tahadlrb7@gmail.com>
2025-12-04 20:32:19 +0330

    feat(users): add updateRoles to repo

--- src/modules/users/application/ports/users.repository.ts
+++ src/modules/users/application/ports/users.repository.ts
@@ -1,10 +1,11 @@
 import { UUID } from 'crypto';
-import { User, UserForCreate, UserPublic } from '../../domain';
+import { User, UserForCreate, UserPublic, UserRole } from '../../domain';
 
 export abstract class UserRepository {
   abstract create(user: UserForCreate): Promise<UserPublic>;
-  abstract getById(id: UUID): Promise<UserPublic | null>;
-  abstract getByEmail(email: string): Promise<UserPublic | null>;
-  abstract getByEmailWithPassword(email: string): Promise<User | null>;
+  abstract getById(id: UUID): Promise<UserPublic>;
+  abstract getByEmail(email: string): Promise<UserPublic>;
+  abstract getByEmailWithPassword(email: string): Promise<User>;
   abstract getAll(): Promise<UserPublic[]>;
+  abstract updateRoles(id: UUID, role: UserRole[]): Promise<UserPublic>;
 }
